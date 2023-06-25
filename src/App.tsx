56601b293465c6c5274a671c3a921aae83345cbb
Jamin <jamincheung1@gmail.com>
2023-06-25 13:49:58 -0400

    Add custom component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
+import Home from './components/home/Home';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-          Learn React
-      </header>
+      <Home />
     </div>
   );
 }
