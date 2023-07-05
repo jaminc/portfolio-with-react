050fa5adb5ae9ad693280db40e64f11b9b7039a9
Jamin <jamincheung1@gmail.com>
2023-07-05 12:20:08 -0400

    Add jobs array data

---
+++ src/data/jobs.ts
@@ -0,0 +1,52 @@
+export const jobs = [
+  {
+    title: "Senior Software Engineer",
+    company: "Capsule",
+    startDate: "2021-12",
+    endDate: "2022-07",
+    bulletPoints: [
+      "Build internal tooling using Node and React for pharmacists and technicians to process and fulfill patient prescriptions.",
+      "Lead a project focused on building the user interface for processing prescriptions. Duties included restructuring the existing codebase, coordinating and prioritizing the product feature requests, and managing the implementation of requirements by breaking down and assigning appropriate tasks to other engineers.",
+    ],
+  },
+  {
+    title: "Lead Software Engineer",
+    company: "Macy's",
+    startDate: "2020-10",
+    endDate: "2021-11",
+    bulletPoints: [
+      "Architect client-side code of product detail pages (PDP) for both Macy’s and Bloomingdale’s to account for reusability and easy mutability due to transient business requirements.",
+      "Coordinate PDP development efforts across all participating parties to ensure proper code execution of all projects within planned timeframes.",
+    ],
+  },
+  {
+    title: "Lean Lab Manager / Senior UI Developer",
+    company: "Bloomingdale's",
+    startDate: "2018-12",
+    endDate: "2020-09",
+    bulletPoints: [
+      "Lead team of 8 comprised of multiple roles to craft and deliver stories for PDP.",
+      "Ensure code quality and maintainability through code reviews.",
+    ],
+  },
+  {
+    title: "UI Developer",
+    company: "Bloomingdale's",
+    startDate: "2017-04",
+    endDate: "2018-12",
+    bulletPoints: [
+      "Work directly with Product Manager, UX, UI, and QA roles in a small lean lab team of 5 to 6 members to A/B test new features and customer experiences. Experiments within mobile lean lab in 2017 resulted in an estimated increase in revenue of $11 million.",
+      "Develop redesigned product detail page for desktop. Project consisted of rewriting client-side code with Backbone/Marionette and adding responsive behavior.",
+    ],
+  },
+  {
+    title: "Junior UI Developer",
+    company: "Bloomingdale's",
+    startDate: "2016-02",
+    endDate: "2017-04",
+    bulletPoints: [
+      "Coordinate with team members across 2-week iterations from initial planning stages with feasibility assessment through to confirmation of successful deployment to production.",
+      "Design and build responsive front-end components with object-oriented approach and supported with behavior driven unit testing. Utilized tools and libraries include javascript ES2015/ES6+, Backbone/Marionette, Jasmine, Sass, Webpack, and Node.",
+    ],
+  },
+];
