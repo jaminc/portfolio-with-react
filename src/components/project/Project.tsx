5370120e8f43eb395b717a2151881710d694129a
Jamin <jamincheung1@gmail.com>
2023-07-05 14:42:49 -0400

    Apply styling to Project

--- src/components/project/Project.tsx
+++ src/components/project/Project.tsx
@@ -2,13 +2,20 @@ import { ReactElement } from "react";
 
 import { Project as ProjectType } from "../../types/projects";
 
+import styles from "./project.module.css";
+
 const Project = ({ project }: { project: ProjectType }): ReactElement => {
   return (
-    <>
-      <h3>{project.title}</h3>
-      <img src={project.imgSrc} alt="" />
-      <div>{project.description}</div>
-    </>
+    <div className={styles.container}>
+      <div>
+        <img src={project.imgSrc} alt="" className={styles.image} />
+      </div>
+
+      <div>
+        <h3>{project.title}</h3>
+        <div>{project.description}</div>
+      </div>
+    </div>
   );
 };
 
