1a6e51bdca8a9650283eb32cf300677ccf376f65
Jamin <jamincheung1@gmail.com>
2023-07-05 15:08:51 -0400

    Set sidebar beside main content

--- src/components/page/Page.tsx
+++ src/components/page/Page.tsx
@@ -3,12 +3,14 @@ import { ReactElement } from "react";
 import Sidebar from "../sidebar/Sidebar";
 import MainContent from "../mainContent/MainContent";
 
+import styles from "./page.module.css";
+
 const Page = (): ReactElement => {
   return (
-    <>
+    <div className={styles.container}>
       <Sidebar />
       <MainContent />
-    </>
+    </div>
   );
 };
 
