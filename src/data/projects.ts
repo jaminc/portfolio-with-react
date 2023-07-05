6d038b2e8d5158a5e011f351e26f304fc4543843
Jamin <jamincheung1@gmail.com>
2023-07-05 13:48:39 -0400

    Add projects data

---
+++ src/data/projects.ts
@@ -0,0 +1,19 @@
+import { Projects } from "../types/projects";
+
+const projects: Projects = [
+  {
+    title: "ActiveArchive",
+    description:
+      "An object-relational-mapping library inspired by ActiveRecord.",
+    sourceCodeLink: "https://github.com/jaminc//ActiveArchive",
+  },
+  {
+    title: "Snake Painter",
+    description:
+      "Game built with JavaScript and inspired by the classic, Snake. Your snake will leave a trail of paint behind. Collect orbs quickly to clean up after yourself. Don't get lost in the paint!",
+    sourceCodeLink: "https://github.com/jaminc//snake",
+    pageLink: "https://github.com/jaminc//ActiveArchive",
+  },
+];
+
+export default projects;
