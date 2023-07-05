eafa88f8cca5965b5e1c1bc5c379422a08f8c206
Jamin <jamincheung1@gmail.com>
2023-07-05 14:47:33 -0400

    Add links to projects

--- src/components/project/Project.tsx
+++ src/components/project/Project.tsx
@@ -7,13 +7,16 @@ import styles from "./project.module.css";
 const Project = ({ project }: { project: ProjectType }): ReactElement => {
   return (
     <div className={styles.container}>
-      <div>
+      <a href={project.pageLink || project.sourceCodeLink}>
         <img src={project.imgSrc} alt="" className={styles.image} />
-      </div>
-
+      </a>
       <div>
         <h3>{project.title}</h3>
         <div>{project.description}</div>
+        <div>
+          <a href={project.sourceCodeLink}>Github Page</a>
+          {project.pageLink && <a href={project.pageLink}>Project Page</a>}
+        </div>
       </div>
     </div>
   );
