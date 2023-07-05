9e4406047a853b943e672f6f1ff9c9d4a51354b6
Jamin <jamincheung1@gmail.com>
2023-07-05 15:17:09 -0400

    Add ids to each content section

--- src/components/contentSection/ContentSection.tsx
+++ src/components/contentSection/ContentSection.tsx
@@ -1,16 +1,18 @@
 import { ReactElement } from "react";
 
 const ContentSection = ({
+  id,
   title,
   className,
   children,
 }: {
+  id: string;
   title: string;
   className?: string;
   children: ReactElement;
 }): ReactElement => {
   return (
-    <section className={className}>
+    <section className={className} id={id}>
       <h2>{title}</h2>
       {children}
     </section>
--- src/components/mainContent/MainContent.tsx
+++ src/components/mainContent/MainContent.tsx
@@ -3,6 +3,8 @@ import { ReactElement } from "react";
 import jobs from "../../data/jobs";
 import projects from "../../data/projects";
 
+import { Link } from "../../types/sidebarLinks";
+
 import ContentSection from "../contentSection/ContentSection";
 import AboutMe from "../aboutMe/AboutMe";
 import Jobs from "../jobs/Jobs";
@@ -13,18 +15,27 @@ import styles from "./main-content.module.css";
 const MainContent = (): ReactElement => {
   return (
     <div>
-      <ContentSection title="About Me" className={styles.mainContentSection}>
+      <ContentSection
+        id={Link.AboutMe}
+        title="About Me"
+        className={styles.mainContentSection}
+      >
         <AboutMe />
       </ContentSection>
 
       <ContentSection
+        id={Link.WorkExperience}
         title="Work Experience"
         className={styles.mainContentSection}
       >
         <Jobs jobs={jobs} />
       </ContentSection>
 
-      <ContentSection title="Projects" className={styles.mainContentSection}>
+      <ContentSection
+        id={Link.Projects}
+        title="Projects"
+        className={styles.mainContentSection}
+      >
         <Projects projects={projects} />
       </ContentSection>
     </div>
