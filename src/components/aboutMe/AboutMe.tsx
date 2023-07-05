b1c14e73f45678b4dcf770629fcffca798ccb01b
Jamin <jamincheung1@gmail.com>
2023-07-05 13:10:13 -0400

    Add AboutMe component

---
+++ src/components/aboutMe/AboutMe.tsx
@@ -0,0 +1,16 @@
+import { ReactElement } from "react";
+
+const AboutMe = (): ReactElement => {
+  return (
+    <section>
+      <h2>About Me</h2>
+      <p>
+        I’m a New York-based web developer experienced in JavaScript, React,
+        Node, and much more. I love the problem solving involved with
+        programming and am eager to further improve my skills.
+      </p>
+    </section>
+  );
+};
+
+export default AboutMe;
